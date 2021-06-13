9c430cdd2ca211c988b95dd5b258488993ba6998
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-06-13 11:43:49 -0300

    App.js now calls API data successfully

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,19 @@
 import React from "react";
+import api from "../api/api";
 
 class App extends React.Component {
+  componentDidMount() {
+    api
+      .get("/weather", {
+        params: {
+          q: "London",
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+      });
+  }
+
   render() {
     return <h1>Hello, World!</h1>;
   }
