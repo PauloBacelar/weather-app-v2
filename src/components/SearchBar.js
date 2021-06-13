67514fdf5be4dabce7d477dac0a7cf6227cf3cb9
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-06-13 18:00:04 -0300

    Styled input text

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from "react";
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>
+      <div className="ui input">
         <input type="text" />
       </div>
     );
