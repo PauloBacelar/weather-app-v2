6356567c4f5256451b3d4191dc5d4a96e12a37ce
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-06-20 15:38:05 -0300

    Header is done (i think)

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -6,25 +6,27 @@ class Weather extends React.Component {
   render() {
     return (
       <div className="weather-card">
-        <div className="header">
-          <div className="icon">
-            <img
-              src={`http://openweathermap.org/img/wn/${this.props.weather.currentWeather.weather[0].icon}@2x.png`}
-              alt={this.props.weather.currentWeather.weather[0].description}
-              className="weather-icon"
-            />
-          </div>
+        <div className="header-container">
+          <div className="header">
+            <div className="icon">
+              <img
+                src={`http://openweathermap.org/img/wn/${this.props.weather.currentWeather.weather[0].icon}@2x.png`}
+                alt={this.props.weather.currentWeather.weather[0].description}
+                className="weather-icon"
+              />
+            </div>
 
-          <div className="text">
-            <h1>
-              {Math.round(this.props.weather.currentWeather.temp)}&#176;C -{" "}
-              {this.props.weather.currentWeather.weather[0].main}
-            </h1>
+            <div className="text">
+              <h1>
+                {Math.round(this.props.weather.currentWeather.temp)}&#176;C -{" "}
+                {this.props.weather.currentWeather.weather[0].main}
+              </h1>
 
-            <div className="place">
-              <p>
-                {this.props.weather.city}, {this.props.weather.country}
-              </p>
+              <div className="place">
+                <p>
+                  {this.props.weather.city}, {this.props.weather.country}
+                </p>
+              </div>
             </div>
           </div>
         </div>
