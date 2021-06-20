2ac9e5d767e60c233afa75c482b24ec7f91678cb
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-06-20 06:52:40 -0300

    Deleted App.css and created Weather.css

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,7 @@ import React from "react";
 class Weather extends React.Component {
   render() {
     return (
-      <div>
+      <div className="weather-card">
         <div>
           <div className="place">
             <h1>
