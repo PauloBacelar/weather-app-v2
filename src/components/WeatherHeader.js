2f78e872775cde216ad58fbcfa38bbaa9e6baf75
Paulo Guilherme Bacelar Andrade <paulobacelara@gmail.com>
2021-06-20 15:46:51 -0300

    Now WeatherHeader component runs the header of the weather card

--- src/components/WeatherHeader.js
+++ src/components/WeatherHeader.js
@@ -2,7 +2,33 @@ import React from "react";
 
 class WeatherHeader extends React.Component {
   render() {
-    return <h1>Hello, World!</h1>;
+    console.log(this.props);
+    return (
+      <div className="header-container">
+        <div className="header">
+          <div className="icon">
+            <img
+              src={`http://openweathermap.org/img/wn/${this.props.data.currentWeather.weather[0].icon}@2x.png`}
+              alt={this.props.data.currentWeather.weather[0].description}
+              className="weather-icon"
+            />
+          </div>
+
+          <div className="text">
+            <h1>
+              {Math.round(this.props.data.currentWeather.temp)}&#176;C -{" "}
+              {this.props.data.currentWeather.weather[0].main}
+            </h1>
+
+            <div className="place">
+              <p>
+                {this.props.data.city}, {this.props.data.country}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
